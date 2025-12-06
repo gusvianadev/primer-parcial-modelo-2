cdfde19d6ab186c763dd26819933d50ae18438e2
Gustavo Viana <gusvianadev@gmail.com>
2025-12-06 14:23:23 -0300

    AAAAAAA

--- src/ej1/ej1.asm
+++ src/ej1/ej1.asm
@@ -1,5 +1,26 @@
 ; Implementación en ASM del ejercicio 1
 global buscarLibrosDisponibles
+extern malloc
+
+section .data
+    ; TODO: Definir offsets y tamaños de estructuras
+    LIBRO_TITULO_OFFSET equ 0
+    LIBRO_AUTOR_OFFSET equ 0
+    LIBRO_ANIO_OFFSET equ 0
+    LIBRO_CATEGORIA_OFFSET equ 0
+    LIBRO_PAGINAS_OFFSET equ 0
+    LIBRO_DISPONIBLE_OFFSET equ 0
+    LIBRO_RATING_OFFSET equ 0
+    LIBRO_SIZE equ 0
+
+    BIBLIO_LIBROS_OFFSET equ 0
+    BIBLIO_CANTIDAD_LIBROS_OFFSET equ 0
+    BIBLIO_NOMBRE_OFFSET equ 0
+    BIBLIO_SIZE equ 0
+
+    LISTA_INDICES_OFFSET equ 0
+    LISTA_CANTIDAD_OFFSET equ 0
+    LISTA_SIZE equ 0
 
 section .text
 
@@ -7,3 +28,4 @@ buscarLibrosDisponibles:
     ; TODO: Implementar
     xor rax, rax
     ret
+
--- src/ej3/ej3.asm
+++ src/ej3/ej3.asm
@@ -1,9 +1,30 @@
 ; Implementación en ASM del ejercicio 3
 global contarPaginasTotales
 
+section .data
+    ; TODO: Definir offsets y tamaños de estructuras
+    LIBRO_TITULO_OFFSET equ 0
+    LIBRO_AUTOR_OFFSET equ 0
+    LIBRO_ANIO_OFFSET equ 0
+    LIBRO_CATEGORIA_OFFSET equ 0
+    LIBRO_PAGINAS_OFFSET equ 0
+    LIBRO_DISPONIBLE_OFFSET equ 0
+    LIBRO_RATING_OFFSET equ 0
+    LIBRO_SIZE equ 0
+
+    BIBLIO_LIBROS_OFFSET equ 0
+    BIBLIO_CANTIDAD_LIBROS_OFFSET equ 0
+    BIBLIO_NOMBRE_OFFSET equ 0
+    BIBLIO_SIZE equ 0
+
+    LISTA_INDICES_OFFSET equ 0
+    LISTA_CANTIDAD_OFFSET equ 0
+    LISTA_SIZE equ 0
+
 section .text
 
 contarPaginasTotales:
     ; TODO: Implementar
     xor rax, rax
     ret
+
